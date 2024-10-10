5f64bde28f97b07dc2d46eb2d8fe752843909e99
gilmore-ec <timgilm@iu.edu>
2024-10-10 19:47:43 -0400

    Add KDoc and link Buttons to Activity

--- app/src/main/java/com/example/to_dolist/AboutActivity.kt
+++ app/src/main/java/com/example/to_dolist/AboutActivity.kt
@@ -1,11 +1,16 @@
 package com.example.to_dolist
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+/**
+ * About activity that displays information about the Developers
+ */
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,14 @@ class AboutActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //Instantiate About page buttons:
+        val backButton: Button = findViewById(R.id.about_back_button)
+
+        //Add listeners to the buttons that launch the related activities
+        backButton.setOnClickListener{ //Closes the Activity
+            Log.i("Buttons","About: Back Button pressed - Closing Activity")
+            finish()
         }
     }
 }
