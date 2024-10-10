10d4b3e8d11350f81c2871e9480667eabf0657a6
gilmore-ec <timgilm@iu.edu>
2024-10-10 19:35:25 -0400

    Add KDoc and link Buttons to Activity - Add KDoc to MainActivity class - Instantiate buttons - Start implementing listeners for buttons

--- app/src/main/java/com/example/to_dolist/MainActivity.kt
+++ app/src/main/java/com/example/to_dolist/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.to_dolist
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+/**
+ * Entry Activity for when the application is launched
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //Instantiate Main Page Buttons:
+        val addButton: Button = findViewById(R.id.add_task_button)
+        val aboutButton: Button = findViewById(R.id.about_button)
+
+        //Add listeners to the buttons that launch the related activities
+        addButton.setOnClickListener{
+            //TODO: launch the AddTaskActivity
+        }
+        aboutButton.setOnClickListener{
+            //TODO: launch the AboutActivity
         }
     }
 }
