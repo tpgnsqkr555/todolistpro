3756bbc979416eecaff8a246b9bb1374199a0583
gilmore-ec <timgilm@iu.edu>
2024-10-11 16:55:24 -0400

    Add features to MainActivity:  - Extend TaskAdapter Class to handle RecyclerView list items  - Add database <tasks> to hold names for tasks  - Add method to update tasks with result from addTaskLauncher  - Initialize and update RecyclerView

--- app/src/main/java/com/example/to_dolist/MainActivity.kt
+++ app/src/main/java/com/example/to_dolist/MainActivity.kt
@@ -10,19 +10,32 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Entry Activity for when the application is launched
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TaskAdapter.RecyclerViewEvent {
+    //Initialize the database and the Adapter
+    val tasks: Array<String?> = arrayOfNulls(7)
+    val taskAdapter = TaskAdapter(tasks, this)
+    var index = 0
+
     //Launcher for TaskActivity results
     private val addTaskLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
             //Handle the response from the TaskActivity
             var received_result = result.data?.getStringExtra("RESULT")
             Log.i("Activities", "MainActivity - Received result: $received_result from TaskActivity")
+            tasks[index] = received_result
+            index++
         }
     }
+    override fun onItemClick(position: Int) {
+        Log.i("Buttons", "MainActivity - Task item $position pressed")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,6 +45,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Initialize the RecyclerView:
+        val recyclerView: RecyclerView = findViewById(R.id.tasks_home)
+        recyclerView.layoutManager = LinearLayoutManager(this) //Set the layout manager type
+        recyclerView.adapter = taskAdapter //Set the Adapter
+
         //Instantiate Main Page Buttons:
         val addButton: Button = findViewById(R.id.add_task_button)
         val aboutButton: Button = findViewById(R.id.about_button)
