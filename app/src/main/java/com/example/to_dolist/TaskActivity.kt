dac6182c24a77a7676f24d7f8904b310b15112ad
gilmore-ec <timgilm@iu.edu>
2024-10-11 15:49:15 -0400

    Add to TaskActivity: - KDoc describing function of activity - Link buttons:    - Save: return correctly entered data to MainActivity and Finish TaskActivity    - Cancel: Finish TaskActivity with no result - Link task_input EditText to TaskActivity - Add LogCat logs for Activities, Buttons, and Values

--- app/src/main/java/com/example/to_dolist/TaskActivity.kt
+++ app/src/main/java/com/example/to_dolist/TaskActivity.kt
@@ -1,12 +1,21 @@
 package com.example.to_dolist
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+/**
+ * Receives data for a new task from the user and returns it to the MainActivity
+ */
 class TaskActivity : AppCompatActivity() {
+    private var acceptableInput: Boolean = false
+    private var result: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,32 @@ class TaskActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //Instantiate Add Task Page Buttons:
+        val saveButton: Button = findViewById(R.id.save_task_button)
+        val cancelButton: Button = findViewById(R.id.cancel_edit_task_button)
+
+        //Instantiate UserInput EditText:
+        val newTaskName: EditText = findViewById(R.id.task_input)
+
+        //Add listeners to the buttons that launch the related activities
+        saveButton.setOnClickListener{
+            Log.i("Buttons","Save button pressed - TaskActivity")
+            result = newTaskName.text.toString()
+            Log.i("Values","New Task: $result")
+            acceptableInput = (result.length in 3..25)
+            if (acceptableInput) {//If the input is acceptable, save the result to the resultIntent
+                Log.i("Activities","TaskActivity - Close with Result: $result")
+                val resultIntent = Intent()
+                resultIntent.putExtra("RESULT",result)
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            } else Log.i("Activities","TaskActivity - Result not valid")
+        }
+        cancelButton.setOnClickListener{
+            Log.i("Buttons","Cancel button pressed - TaskActivity")
+            Log.i("Activities","TaskActivity - Close without Result")
+            finish()
         }
     }
 }
