19ca9d77dec2b875d38c998fccb8ec2d1cdfb24b
gilmore-ec <timgilm@iu.edu>
2024-10-13 16:33:37 -0400

    Add: - Add data restoration - Add callback overrides    - Log values    - Save data    - Restore data
    Fix:
    - Change Database (tasks) from Array to ArrayList
    - Change index value limit to <7

--- app/src/main/java/com/example/to_dolist/MainActivity.kt
+++ app/src/main/java/com/example/to_dolist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.to_dolist
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -18,8 +19,7 @@ import androidx.recyclerview.widget.RecyclerView
  */
 class MainActivity : AppCompatActivity(), TaskAdapter.RecyclerViewEvent {
     //Initialize the database and the Adapter
-    val tasks: Array<String?> = arrayOfNulls(7)
-    val taskAdapter = TaskAdapter(tasks, this)
+    var tasks: ArrayList<String> = ArrayList()
     var index = 0
 
     //Launcher for TaskActivity results
@@ -28,15 +28,21 @@ class MainActivity : AppCompatActivity(), TaskAdapter.RecyclerViewEvent {
             //Handle the response from the TaskActivity
             var received_result = result.data?.getStringExtra("RESULT")
             Log.i("Activities", "MainActivity - Received result: $received_result from TaskActivity")
-            tasks[index] = received_result
-            index++
+            if (received_result != null && index < 7) {
+                tasks.add(received_result)
+                index++
+            }
         }
     }
+    @Override
     override fun onItemClick(position: Int) {
         Log.i("Buttons", "MainActivity - Task item $position pressed")
     }
 
+    @Override
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("Activities", "MainActivity - onCreate called")
+        Log.i("Values", "MainActivity - index is $index")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -48,7 +54,7 @@ class MainActivity : AppCompatActivity(), TaskAdapter.RecyclerViewEvent {
         //Initialize the RecyclerView:
         val recyclerView: RecyclerView = findViewById(R.id.tasks_home)
         recyclerView.layoutManager = LinearLayoutManager(this) //Set the layout manager type
-        recyclerView.adapter = taskAdapter //Set the Adapter
+        recyclerView.adapter = TaskAdapter(tasks, this) //Set the Adapter
 
         //Instantiate Main Page Buttons:
         val addButton: Button = findViewById(R.id.add_task_button)
@@ -64,6 +70,103 @@ class MainActivity : AppCompatActivity(), TaskAdapter.RecyclerViewEvent {
             Log.i("Buttons","About button pressed - MainActivity")
             val aboutIntent = Intent(this, AboutActivity::class.java)
             startActivity(aboutIntent)
+        }
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        index = sharedPref.getString("INDEX", "0").toString().toInt()
+        for (i:Int in 0..index) {
+            if (sharedPref.getString("TASK $i", "Issue").toString() != "Issue") {
+                tasks.add(sharedPref.getString("TASK $i", "Issue...").toString())
+            }
+        }
+    }
+
+    @Override
+    override fun onStart() {
+        super.onStart()
+        Log.i("Activities", "MainActivity - onStart called")
+        Log.i("Values", "MainActivity - index is $index")
+    }
+
+    @Override
+    override fun onRestart() {
+        Log.i("Activities", "MainActivity - onRestart called")
+        Log.i("Values", "MainActivity - index is $index")
+        super.onRestart()
+    }
+
+    @Override
+    override fun onResume() {
+        Log.i("Activities", "MainActivity - onResume called")
+        Log.i("Values", "MainActivity - index is $index")
+        super.onResume()
+        val recyclerView: RecyclerView = findViewById(R.id.tasks_home)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = TaskAdapter(tasks, this) //Set the Adapter
+    }
+
+    @Override
+    override fun onPause() {
+        Log.i("Activities", "MainActivity - onPause called")
+        Log.i("Values", "MainActivity - index is $index")
+        super.onPause()
+    }
+
+    @Override
+    override fun onStop() {
+        super.onStop()
+        Log.i("Activities", "MainActivity - onStop called")
+        Log.i("Values", "MainActivity - index is $index")
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()) {
+            putString("INDEX","$index")
+            for (i:Int in 0..index) {
+                putString("TASK $i", tasks[i])
+            }
+            apply()
+        }
+    }
+
+    @Override
+    override fun onDestroy() {
+        Log.i("Activities", "MainActivity - onDestroy called")
+        super.onDestroy()
+    }
+
+    @Override
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("Activities", "MainActivity - onSaveInstanceState called")
+        Log.i("Values", "MainActivity - index is $index")
+        // Save the user's current game state.
+        outState.run {
+            for (i: Int in 0..index) {
+                putString("TASK_$i", tasks[i])
+            }
+            putString("INDEX","$index")
+        }
+        // Always call the superclass so it can save the view hierarchy state.
+        super.onSaveInstanceState(outState)
+    }
+    companion object {
+        val INDEX = "INDEX"
+        val TASK_0 = "TASK_0"
+        val TASK_1 = "TASK_1"
+        val TASK_2 = "TASK_2"
+        val TASK_3 = "TASK_3"
+        val TASK_4 = "TASK_4"
+        val TASK_5 = "TASK_5"
+        val TASK_6 = "TASK_6"
+    }
+    @Override
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.i("Activities", "MainActivity - onRestoreInstanceState called")
+        Log.i("Values", "MainActivity - index is $index")
+        super.onRestoreInstanceState(savedInstanceState)
+        // Restore tasks from saved instance.
+        savedInstanceState.run {
+            index = getString("INDEX","0").toString().toInt()
+            for (i:Int in 0..index) {
+                tasks.add(getString("TASK_$i","").toString())
+            }
         }
     }
 }
